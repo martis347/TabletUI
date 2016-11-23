b9afd0fd171f4d209e84a58b83e5caa64c143e45
Martynas Kanapinskas <martis347@gmail.com>
2016-11-24 00:57:43 +0200

    Changed how coordinated are taken

--- js/weather.js
+++ js/weather.js
@@ -1,29 +1,22 @@
-if ("geolocation" in navigator) {
-    navigator.geolocation.getCurrentPosition(function (position) {
-        loadWeather(position.coords.latitude+','+position.coords.longitude);
-    });
-} else{
-    loadWeather("Bangalore,IN","");
-}
-
 $(document).ready(function(){
-	loadWeather(position.coords.latitude+','+position.coords.longitude);
-
-    /*setInterval(function(){navigator.geolocation.getCurrentPosition(function (position) {
-        loadWeather(position.coords.latitude+','+position.coords.longitude);
-    })},10000);*/
+	var latitude = QueryString.latitude === undefined ? '54.864655' : QueryString.latitude;
+	var longitude = QueryString.longitude === undefined ? '23.962950799999998' : QueryString.longitude;
+	var location = latitude + ',' + longitude;
+	console.log(location);
+	
+	loadWeather(location);
+    setInterval(function(){loadWeather(location)}, 1000);
 });
 
-function loadWeather(location, woeid){
+function loadWeather(location){
     $.simpleWeather({
         location: location,
-        woeid: woeid,
         unit: 'C',
         success: function(weather){
             city = weather.city;
             temp = weather.temp + '&deg;';
-            wcode = '<img class="weathericon" src="images/weathericons/'+weather.code+'.svg">';
-            wind = '<p>'+weather.wind.speed + ' ' + weather.units.speed+'</p>';
+            wcode = '<svg class="weathericon" src="images/weathericons/'+weather.code+'.svg">';
+            wind = '<p>' + weather.wind.speed + weather.units.speed + '</p>';
             humidity = weather.humidity+' %';
             $(".location").text(city);
             $(".temperature").html(temp);
@@ -37,3 +30,29 @@ function loadWeather(location, woeid){
         
     });
 }
+
+
+
+
+var QueryString = function () {
+  // This function is anonymous, is executed immediately and 
+  // the return value is assigned to QueryString!
+  var query_string = {};
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i=0;i<vars.length;i++) {
+    var pair = vars[i].split("=");
+        // If first entry with this name
+    if (typeof query_string[pair[0]] === "undefined") {
+      query_string[pair[0]] = decodeURIComponent(pair[1]);
+        // If second entry with this name
+    } else if (typeof query_string[pair[0]] === "string") {
+      var arr = [ query_string[pair[0]],decodeURIComponent(pair[1]) ];
+      query_string[pair[0]] = arr;
+        // If third or later entry with this name
+    } else {
+      query_string[pair[0]].push(decodeURIComponent(pair[1]));
+    }
+  } 
+  return query_string;
+}();
